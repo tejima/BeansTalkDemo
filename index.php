48968aa2a22f19791d6473a840108b011a664c8f
手嶋守 <tejima@gmail.com>
2013-02-08 18:29:50 +0900

    edit index

--- index.php
+++ index.php
@@ -12,33 +12,8 @@
 	<link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
 <body>
-	<section class="congratulations">
-		<h1>Congratulations!</h1>
-		<p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-		<p>You are running PHP version <?php echo phpversion(); ?></p>
-		<aside>(&nbsp;<a href="/info.php">View PHP information</a>&nbsp;)</aside>
-	</section>
 
-	<section class="instructions">
-		<h2>What's Next?</h2>
-		<ul>
-			<li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
-<li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/index.html?concepts.html">AWS Elastic Beanstalk concepts</a></li>
-<li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_PHP.html">Deploying Applications in PHP Using Git</a></li>
-<li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/index.html?create_deploy_PHP.container.html">Managing PHP Environment Settings </a></li>
-<li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/using-features.loggingS3.title.html">Working with Logs </a></li>
-		</ul>
+	OpenPNE Application 
 
-		<h2>AWS SDK for PHP</h2>
-		<ul>
-			<li><a href="http://aws.amazon.com/sdkforphp">AWS SDK for PHP home</a></li>
-			<li><a href="http://aws.amazon.com/php">PHP developer center</a></li>
-			<li><a href="http://aws.amazon.com/articles/4261?_encoding=UTF8&jiveRedirect=1">Getting started guide</a></li>
-			<li><a href="http://aws.amazon.com/documentation/sdkforphp">AWS SDK for PHP documentation</a></li>
-			<li><a href="https://github.com/amazonwebservices/aws-sdk-for-php">AWS SDK for PHP on GitHub</a></li>
-		</ul>
-	</section>
-
-	<!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
 </body>
 </html>
